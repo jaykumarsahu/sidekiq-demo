bd755d2d81db0e87e31c4bc88547537cfd8e6e17
Jay Sahu <jsahu.ror@gmail.com>
2020-12-08 11:54:56 +0530

    quick fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,3 @@
 # frozen_string_literal: true
 
 require 'sidekiq/web'
-
-
-Sidekiq.configure_server do |config|
-    config.options[:fetch] = Sidekiq::PriorityQueue::Fetch
-  end
-
-  Sidekiq.configure_client do |config|
-    config.client_middleware do |chain|
-      chain.add Sidekiq::PriorityQueue::Client
-  end
-end
